39a051b6b1690e870a06cb1325f05cba73082718
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-06-20 02:10:29 +0700

    add turbo_frame_tag for hotels feature

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,2 +1,11 @@
 class Hotel < ApplicationRecord
+  # Relationships
+
+  # Validates
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :phone, presence: true
+  validates :check_in_time, presence: true
+  validates :check_out_time, presence: true
+
 end
