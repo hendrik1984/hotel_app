c810401af2ebb8f9e07d9cab432ebb041351c84c
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-06-24 13:43:01 +0700

    update edit delete redirect page

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -25,11 +25,10 @@ class HotelsController < ApplicationController
 
     respond_to do |format|
       if @hotel.save
-        format.html { redirect_to hotel_url(@hotel), notice: "Hotel was successfully created." }
-        format.json { render :show, status: :created, location: @hotel }
+        format.html { redirect_to hotels_url(@hotel), notice: "Hotel was successfully created." }
+
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @hotel.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -38,11 +37,11 @@ class HotelsController < ApplicationController
   def update
     respond_to do |format|
       if @hotel.update(hotel_params)
-        format.html { redirect_to hotel_url(@hotel), notice: "Hotel was successfully updated." }
-        format.json { render :show, status: :ok, location: @hotel }
+        format.html { redirect_to hotels_url(@hotel), notice: "Hotel was successfully updated." }
+        
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @hotel.errors, status: :unprocessable_entity }
+
       end
     end
   end
