70274db237b75648c939ca717e9872750739d984
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-06-24 11:42:48 +0700

    remove connections

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -3,6 +3,5 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="menu"
 export default class extends Controller {
   connect() {
-    console.log("connecting to menu stimulus...")
   }
 }
