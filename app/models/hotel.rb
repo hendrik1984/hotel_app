7e8160c151e3c9c0a50f599860693e9381e0cd79
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-06-24 14:34:37 +0700

    update view stars using image

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -7,5 +7,5 @@ class Hotel < ApplicationRecord
   validates :phone, presence: true
   validates :check_in_time, presence: true
   validates :check_out_time, presence: true
-
+  validates :stars, numericality: {greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
 end
