89fb3a9374d2a589c87c4bab7999a0ca3454caa0
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-06-27 14:32:29 +0700

    add devise views for customization page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :hotels
-  post 'search', to: 'hotels#search'
-  devise_for :users
-  root to: "home#index"
-  get 'dashboard', to: 'home#dashboard'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Root
+  root to: "home#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # List Resources
+  resources :hotels
+  
+  # Devise
+  devise_for :users
+
+  # Specific page
+  post 'search', to: 'hotels#search'
+  get 'dashboard', to: 'home#dashboard'
 end
