d0e3343b485700072c0da277a26ec02dbf855072
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2024-07-08 20:36:50 +0700

    update schema for descriptions

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_08_132340) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "geolocation"
-    t.string "desriptions"
+    t.string "descriptions"
     t.string "country"
     t.integer "score"
   end
